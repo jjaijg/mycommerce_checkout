2a0981fa22ec00d1d1f03d8eccc173cb481eb668
jjaijg <jjaiganeshjg95@gmail.com>
2025-06-24 09:59:49 +0530

    updated checkout form logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+// Declarative router
 // import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  // <BrowserRouter>
+  <StrictMode>
     <App />
-  // {/* </BrowserRouter> */}
+  </StrictMode>
 );
